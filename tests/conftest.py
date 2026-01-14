a859eee5e089fe1f71db9db52dc497f7478c7aed
Matt Clare <matt@mattclare.ca>
2026-01-13 23:14:29 -0500

    A little more info in tests/conftest.py to help on the github side

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,16 @@
-pytest_plugins = [
-	"pytest_asyncio",
-	"pytest_homeassistant_custom_component",
-]
+# Load pytest plugins if available to avoid CI import errors when dev
+# dependencies are not installed. CI should install test deps (see
+# the workflow snippet in the README or below), but this makes the
+# test suite more robust when pytest-asyncio isn't present.
+pytest_plugins = []
+
+try:
+	import pytest_asyncio  # type: ignore
+
+	pytest_plugins.append("pytest_asyncio")
+except Exception:
+	# pytest_asyncio not installed; skip adding it so pytest won't error
+	# when importing plugins listed in `pytest_plugins`.
+	pass
+
+pytest_plugins.append("pytest_homeassistant_custom_component")
