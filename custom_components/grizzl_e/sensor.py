8acd3f0d8c5607e94536bb06796fe9cedb66b676
Matt Clare <matt@mattclare.ca>
2026-09-20 22:50:33 -0400

    Energy tracking fixes

--- custom_components/grizzl_e/sensor.py
+++ custom_components/grizzl_e/sensor.py
@@ -43,8 +43,8 @@ async def async_setup_entry(hass, entry, async_add_entities):
         GrizzleESensor(coordinator, "Temperature 1", "temperature1", UnitOfTemperature.CELSIUS, device_class=SensorDeviceClass.TEMPERATURE),
         GrizzleESensor(coordinator, "Temperature 2", "temperature2", UnitOfTemperature.CELSIUS, device_class=SensorDeviceClass.TEMPERATURE),
         GrizzleESensor(coordinator, "RSSI", "RSSI", "dBm", device_class=SensorDeviceClass.SIGNAL_STRENGTH, entity_category=EntityCategory.DIAGNOSTIC),
-        GrizzleESensor(coordinator, "State", "state", None, device_class=SensorDeviceClass.ENUM, options=["PowerUp", "SelfTest", "Standby", "Vehicle Connected", "Vehicle Charging", "Charging Complete", "Disabled", "Error"]),
-        GrizzleESensor(coordinator, "Pilot State", "pilot", None, device_class=SensorDeviceClass.ENUM, options=["no_ev", "ev_connected"]),
+        GrizzleESensor(coordinator, "State", "state", None, device_class=SensorDeviceClass.ENUM, state_class=None, options=["PowerUp", "SelfTest", "Standby", "Vehicle Connected", "Vehicle Charging", "Charging Complete", "Disabled", "Error"]),
+        GrizzleESensor(coordinator, "Pilot State", "pilot", None, device_class=SensorDeviceClass.ENUM, state_class=None, options=["no_ev", "ev_connected"]),
         GrizzleESensor(coordinator, "Session Time", "sessionTime", UnitOfTime.SECONDS, device_class=SensorDeviceClass.DURATION),
         # Diagnostic and version information
         GrizzleESensor(coordinator, "EVSE Version", "verFWMain", None, entity_category=EntityCategory.DIAGNOSTIC, state_class=None),
@@ -72,7 +72,7 @@ async def async_setup_entry(hass, entry, async_add_entities):
         add("power", "Power", UnitOfPower.WATT, device_class=SensorDeviceClass.POWER)
         add("current", "Current", UnitOfElectricCurrent.AMPERE, device_class=SensorDeviceClass.CURRENT)
         add("voltage", "Voltage", UnitOfElectricPotential.VOLT, device_class=SensorDeviceClass.VOLTAGE)
-        add("session_energy", "Session Energy", UnitOfEnergy.KILO_WATT_HOUR, device_class=SensorDeviceClass.ENERGY)
+        add("session_energy", "Session Energy", UnitOfEnergy.KILO_WATT_HOUR, device_class=SensorDeviceClass.ENERGY, state_class=SensorStateClass.TOTAL_INCREASING)
         add("total_energy", "Total Energy", UnitOfEnergy.KILO_WATT_HOUR, device_class=SensorDeviceClass.ENERGY, state_class=SensorStateClass.TOTAL_INCREASING)
         add("session_time", "Session Time", UnitOfTime.SECONDS, device_class=SensorDeviceClass.DURATION)
         add("state", "State", None, device_class=SensorDeviceClass.ENUM, state_class=None, options=state_options)
